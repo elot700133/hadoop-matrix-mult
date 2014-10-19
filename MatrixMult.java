1d776b1c2f1704efcb3f8a47e989e9a2733c77e9
Too Lee <hojotoolee2@gmail.com>
2014-10-19 14:31:54 -0400

    add combiner

--- MatrixMult.java
+++ MatrixMult.java
@@ -44,6 +44,38 @@ public class MatrixMult{
       }
     }
  
+    public static class Combiner extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+          if(false){
+            String[] value;
+            HashMap<Integer, Float> hashA = new HashMap<Integer, Float>();
+            HashMap<Integer, Float> hashB = new HashMap<Integer, Float>();
+            for (Text val : values) {
+                value = val.toString().split(",");
+                if (value[0].equals("A")) {
+                    hashA.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+                } else {
+                    hashB.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+                }
+            }
+            int n = Integer.parseInt(context.getConfiguration().get("n"));
+            float result = 0.0f;
+            float a_ij;
+            float b_jk;
+            for (int j = 0; j < n; j++) {
+                a_ij = hashA.containsKey(j) ? hashA.get(j) : 0.0f;
+                b_jk = hashB.containsKey(j) ? hashB.get(j) : 0.0f;
+                result += a_ij * b_jk;
+            }
+            if (result != 0.0f) {
+                context.write(null, new Text(key.toString() + "," + Float.toString(result)));
+            }
+        } //if
+        //Text outputValue = new Text();
+        //outputValue = values.iterator().next();
+        context.write(key,new Text(values.iterator().next()+",combiner"));
+        }
+    }
     public static class Reduce extends Reducer<Text, Text, Text, Text> {
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
           if(false){
@@ -73,7 +105,7 @@ public class MatrixMult{
         } //if
         //Text outputValue = new Text();
         //outputValue = values.iterator().next();
-        context.write(key,new Text(values.iterator().next()));
+        context.write(key,new Text(values.iterator().next()+",reducer"));
         }
     }
  
@@ -90,6 +122,7 @@ public class MatrixMult{
         job.setOutputValueClass(Text.class);
  
         job.setMapperClass(Map.class);
+        job.setCombinerClass(Combiner.class);
         job.setReducerClass(Reduce.class);
  
         job.setInputFormatClass(TextInputFormat.class);
