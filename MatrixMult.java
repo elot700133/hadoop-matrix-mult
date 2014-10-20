07af765e0d30b1113f25702ff09f24b62a9f0954
Too Lee <hojotoolee2@gmail.com>
2014-10-20 00:33:53 -0400

    removed alot of debug stuff

--- MatrixMult.java
+++ MatrixMult.java
@@ -20,29 +20,6 @@ public class MatrixMult{
     public static class Map extends Mapper<LongWritable, Text, Text, Text> {
       public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if(false){
-            Configuration conf = context.getConfiguration();
-            int m = Integer.parseInt(conf.get("m"));
-            int p = Integer.parseInt(conf.get("p"));
-            String line = value.toString();
-            String[] indicesAndValue = line.split(",");
-            Text outputKey = new Text();
-            Text outputValue = new Text();
-            if (indicesAndValue[0].equals("A")) {
-                for (int k = 0; k < p; k++) {
-                    outputKey.set(indicesAndValue[1] + "," + k);
-                    outputValue.set("A," + indicesAndValue[2] + "," + indicesAndValue[3]);
-                    context.write(outputKey, outputValue);
-                }
-            } else {
-                for (int i = 0; i < m; i++) {
-                    outputKey.set(i + "," + indicesAndValue[2]);
-                    outputValue.set("B," + indicesAndValue[1] + "," + indicesAndValue[3]);
-                    context.write(outputKey, outputValue);
-                }
-            }
-        }
-
-        if(false){
           Text outputKey = new Text();
           outputKey.set(key.toString());
           context.write(outputKey,value);
@@ -92,50 +69,12 @@ public class MatrixMult{
     public static class Combiner extends Reducer<Text, Text, Text, Text> {
       public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         if(false){
-            String[] value;
-            HashMap<Integer, Float> hashA = new HashMap<Integer, Float>();
-            HashMap<Integer, Float> hashB = new HashMap<Integer, Float>();
-            for (Text val : values) {
-                value = val.toString().split(",");
-                if (value[0].equals("A")) {
-                    hashA.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
-                } else {
-                    hashB.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
-                }
-            }
-            int n = Integer.parseInt(context.getConfiguration().get("n"));
-            float result = 0.0f;
-            float a_ij;
-            float b_jk;
-            for (int j = 0; j < n; j++) {
-                a_ij = hashA.containsKey(j) ? hashA.get(j) : 0.0f;
-                b_jk = hashB.containsKey(j) ? hashB.get(j) : 0.0f;
-                result += a_ij * b_jk;
-            }
-            if (result != 0.0f) {
-                context.write(null, new Text(key.toString() + "," + Float.toString(result)));
-            }
-        } //if
-
-        if(false){
           Text outputValue = new Text();
           //outputValue = values.iterator().next();
           int i=0;
           while(values.iterator().hasNext()){
             context.write(key,new Text(values.iterator().next()+",combiner"+Integer.toString(i)));
             i++;
-          }
-        }
-        if(false){ 
-          List<Text> copy = new ArrayList<Text>();
-          while(values.iterator().hasNext()){
-            copy.add(new Text(values.iterator().next()));
-          }
-          for(int i=0; i<copy.size();i++){
-            context.write(key,new Text(copy.get(i)+",combiner"+Integer.toString(i)));
-          }
-          for(int i=0; i<copy.size();i++){
-            context.write(key,new Text(copy.get(i)+",combiner"+Integer.toString(i+10)));
           }
         }
         if(true){
@@ -152,8 +91,6 @@ public class MatrixMult{
           // get the output key ready
           // it is row of A, col of B
           // each of the element of A need to multiply all the B elements
-          //Iterator<Text> iter_i = values.iterator();
-          //while(iter_i.hasNext()){
           for(int i=0;i<copy.size();i++){
             // setup a line of file
             //String line_i = iter_i.next().toString();
@@ -161,40 +98,12 @@ public class MatrixMult{
             String[] indicesAndValue_i = line_i.split(",");
             // find matrix A
             if(indicesAndValue_i[mIdx].equals("A")){
-              if(false){
-                // setup outputKey and outputValue
-                Text outputKey = new Text();
-                Text outputValue = new Text();
-                outputKey.set(indicesAndValue_i[rIdx]); // row of A, col of B
-                context.write(outputKey,new Text("i am A"));
-                continue;
-              }
-              //Iterable<Text> values2 = values;
-              //Iterator<Text> iter_j = values2.iterator();
-              //while(iter_j.hasNext()){
               for(int j=0;j<copy.size();j++){
                 // setup a line of file
-                //String line_j = iter_j.next().toString();
                 String line_j = copy.get(j).toString();
                 String[] indicesAndValue_j = line_j.split(",");
-                if(false){
-                  // setup outputKey and outputValue
-                  Text outputKey = new Text();
-                  Text outputValue = new Text();
-                  //outputKey.set(); // row of A, col of B
-                  context.write(new Text(line_i),new Text(line_j));
-                  continue;
-                }
                 // multiply each matrix B
                 if(indicesAndValue_j[mIdx].equals("B")){
-                  if(false){
-                    // setup outputKey and outputValue
-                    Text outputKey = new Text();
-                    Text outputValue = new Text();
-                    outputKey.set(indicesAndValue_i[rIdx] + "," + indicesAndValue_j[cIdx]); // row of A, col of B
-                    context.write(outputKey,new Text("i am B"));
-                    continue;
-                  }
                   // setup outputKey and outputValue
                   Text outputKey = new Text();
                   Text outputValue = new Text();
@@ -208,9 +117,9 @@ public class MatrixMult{
                 }
               }
             }
-          }
-        }
-      }
+          }//for
+        }//if(true)
+      }//reduce
     }
 
     // 3rd phase:
